e78da68d046b8e770adcb7b96f5f137d05e2628e
berckan <berckanplay@gmail.com>
2026-01-16 18:37:24 -0500

    actualizado conexion db.js

--- db.js
+++ db.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql2');
 
-const conn = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'Mirandatkm76476541@',   
-  database: 'ferreteria_db'
+const conn = mysql.createConnection({ 
+  host: process.env.DB_HOST, 
+  port: process.env.DB_PORT || 3306, 
+  user: process.env.DB_USER, 
+  password: process.env.DB_PASSWORD, 
+  database: process.env.DB_NAME 
 });
 
 conn.connect(err => {
