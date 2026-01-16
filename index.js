8ef3d69cf983c09081c679c12e00104c3cf99810
berckan <berckanplay@gmail.com>
2026-01-16 18:39:26 -0500

    actualizado index.js

--- index.js
+++ index.js
@@ -14,6 +14,8 @@ app.use('/clientes', clientesRoutes);
 app.use('/productos', productosRoutes);
 app.use('/ventas', ventasRoutes);
 
-app.listen(3000, () => {
-  console.log('Servidor corriendo en http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en puerto ${PORT}`);
 });
+
